034b4979018782ccfb5b13484f65b2b16bf0734a
annlie <byguk@aol.com>
2020-12-05 18:39:52 -0500

    new contact page with functionality

---
+++ yeuu/script.js
@@ -0,0 +1,35 @@
+let sendMessage = document.getElementbyId("name");
+
+$(document).ready(function() {
+	// $("#button").click(function() {
+	// 	// $("#contact").hide();
+	// 	// $("#cForm").css("visibility", "hidden");
+
+	// 	$("#contact").hide();
+
+	// 	var p = document.createElement("p");
+	// 	var message = document.createTextNode("Thank you for your message.");
+	// 	p.appendChild(message);
+
+	// 	var div = document.getElementbyId("contact");
+	// 	div.appendChild(p);
+	// });
+
+	// $("#button").click(function() {
+	// 	// alert("thank you for your message");
+	// 	sendMessage.innerHTML = "annlie";
+	// });
+});
+
+//notes:
+//classes use . (multiple elements)
+//ids use # (one element)
+
+// var box = $(".box");
+// box.animate({width: '40vw'}, "slow");
+
+
+$("#button").click(function() {
+		// alert("thank you for your message");
+		sendMessage.innerHTML = "annlie";
+	});
